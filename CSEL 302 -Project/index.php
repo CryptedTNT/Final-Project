d9066042d9c39a57bea215c86cef2a75cc768496
Soje360 <133828231+Soje360@users.noreply.github.com>
2025-05-11 22:18:13 +0800

    Week 1 (UI and Design)
    Week 1

---
+++ CSEL 302 -Project/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>X Post Emotion Classifier</title>
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://kit.fontawesome.com/4f7949d798.js" crossorigin="anonymous"></script>
+</head>
+
+<!-- STYLE -->
+   
+<!-- BODY -->
+<body>
+
+
+<div id="outer_container">
+
+    <!-- Left Section -->
+    <div class="left_section">
+
+        <div class="Title_Text">
+           <h1> TWITTER POST EMOTION CLASSIFIER</h1>
+        </div>
+
+        <!-- tools option -->
+        <div class="toolbar_div">
+            <i class="fa-solid fa-bars fa-2x" ></i>
+        </div>
+
+        <!-- textbox and the submit button -->
+        <input type="text" class="input_box" placeholder="Type or Paste the link here...">
+        <button class="submit_button">Submit</button>
+
+        <!-- another button for history -->
+        <div class="history">
+            <i class="fa-solid fa-clock-rotate-left fa-2x"></i>
+        </div>
+
+        <div class = "light_dark">
+              <i class="fa-solid fa-moon fa-2x"></i>
+        </div>
+
+    </div>
+    
+    <!-- Right Section -->
+    <div class="right_section">
+        <div class="close_button">
+            <i class="fa-solid fa-circle-xmark fa-2x"></i>
+        </div>
+    </div>
+
+</div> 
+
+
+</body>
+</html>
